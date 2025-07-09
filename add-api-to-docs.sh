e2de238150d81ce8cc6a3ffee336a9383f6519e1
Jan van Mansum <janvanmansum@gmail.com>
2025-07-09 15:16:29 +0200

    Fix api docs

--- add-api-to-docs.sh
+++ add-api-to-docs.sh
@@ -14,8 +14,13 @@
 # limitations under the License.
 #
 
-mvn dans-build-resources:get-helper-script
-sh target/add-swagger-ui.sh
+# Fail on error
+set -e
 
-mvn initialize
-cp target/openapi/dd-data-vault-api.yml docs/api.yml
+mvn dans-build-resources:get-helper-script
+mvn initialize # To ensure API definition is downloaded
+
+echo "Deploying Swagger UI and API definition..."
+sh target/add-swagger-ui.sh
+cp target/openapi/*.yml docs/api.yml
+echo "DONE"
